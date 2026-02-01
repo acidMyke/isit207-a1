879522845f8c2e0d4ad130c4b1cd3c319b907649
Jun Jie <jjyong004@mymail.sim.edu.sg>
2026-02-01 20:42:11 +0800

    feat: common formStatus

--- main.js
+++ main.js
@@ -11,6 +11,32 @@ const accounts = [];
 /** @type {Account | null} */
 const currentAccount = null;
 
+function setFormStatus(status, type = 'error', el) {
+  /** @type {HTMLDivElement | null} */
+  const formStatusEl = (el ?? document).querySelector('.formStatus');
+
+  if (!formStatusEl) {
+    throw new Error('Form status element not found');
+  }
+
+  if (status === undefined || status === null) {
+    formStatusEl.innerHTML = '';
+  }
+
+  if (!Array.isArray(status)) {
+    status = [status];
+  }
+
+  for (let s of status) {
+    if (typeof s == 'string') {
+      const pEl = document.createElement('p');
+      pEl.classList.add(type);
+      pEl.innerText = status;
+      formStatusEl.appendChild(pEl);
+    }
+  }
+}
+
 /**
  *
  * @param {SubmitEvent} event
@@ -19,8 +45,26 @@ function processLogin(event) {
   if (!event || !event.target) return;
   event.preventDefault();
   const data = new FormData(event.target);
-  const namail = data.get('namail');
+  const namail = data.get('namail').trim();
   const password = data.get('password');
+
+  if (namail.length === 0) {
+    setFormStatus('Name/Email cannot be empty');
+    return;
+  }
+
+  for (let xAcc of accounts) {
+    if (xAcc.name === namail || xAcc.email === namail) {
+      setCurrentAccount(xAcc);
+    }
+  }
+
+  setFormStatus('Account not found');
+}
+
+/** @param {Account} */
+function setCurrentAccount(account) {
+  currentAccount = account;
 }
 
 /**
@@ -31,7 +75,19 @@ function processSignUp(event) {
   if (!event || !event.target) return;
   event.preventDefault();
   const data = new FormData(event.target);
-  const name = data.get('name');
-  const email = data.get('email');
+  const name = data.get('name').trim();
+  const email = data.get('email').trim();
   const password = data.get('password');
+
+  for (let xAcc of accounts) {
+    if (xAcc.name === name) {
+      setFormStatus('name used!!');
+      return;
+    }
+
+    if (xAcc.email === email) {
+      setFormStatus('email used!!');
+      return;
+    }
+  }
 }
